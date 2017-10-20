c2b9eb7355816d2c374441247f7dc3938ac30588
daftMarc <marc.familari@gmail.com>
2017-10-20 14:11:50 +0200

    inital commit

---
+++ SwiftyProteins/SwiftyProteins/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyProteins
+//
+//  Created by Marc FAMILARI on 10/20/17.
+//  Copyright © 2017 Marc FAMILARI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
