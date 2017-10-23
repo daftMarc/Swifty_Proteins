49e0c54d83198f904bf24b365709943ebb380039
daftMarc <marc.familari@gmail.com>
2017-10-23 15:48:08 +0200

    pending: working on UISearchController

--- SwiftyProteins/SwiftyProteins/LigandTableViewController.swift
+++ SwiftyProteins/SwiftyProteins/LigandTableViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class LigandTableViewController: UITableViewController {
+class LigandTableViewController: UITableViewController, UISearchBarDelegate, UISearchControllerDelegate {
     
     
-    let data: [String] = Data.ligands
+    let ligands: [String] = Data.ligands
+    var filteredLigands: [String]?
     
     
     override func viewDidLoad() {
@@ -24,15 +25,31 @@ class LigandTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.data.count
+        return self.ligands.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        cell.textLabel?.text = self.data[indexPath.row]
+        cell.textLabel?.text = self.ligands[indexPath.row]
         
         return cell
+    }
+    
+    
+    
+    
+    // MARK: - UISearchBar
+    
+    func searchDisplayController(controller: UISearchController!, shouldReloadTableForSearchString searchString: String!) -> Bool {
+        self.filterContentForSearchText(searchText: searchString)
+        return true
+    }
+    
+    func filterContentForSearchText(searchText: String) {
+        self.filteredLigands = self.ligands.filter({( name: String) -> Bool in
+            return name.lowercased().range(of: searchText.lowercased()) != nil
+        })
     }
 
 }
