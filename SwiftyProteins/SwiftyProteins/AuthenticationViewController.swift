09906b68c2c249a0337efcd83d8899924e1b7bf1
daftMarc <marc.familari@gmail.com>
2017-10-23 11:51:03 +0200

    add touchID

--- SwiftyProteins/SwiftyProteins/AuthenticationViewController.swift
+++ SwiftyProteins/SwiftyProteins/AuthenticationViewController.swift
@@ -15,6 +15,7 @@ class AuthenticationViewController: UIViewController {
     @IBAction func loginAction(_ sender: UIButton) {
     }
     @IBAction func touchIDAction(_ sender: UIButton) {
+        self.authenticateUser()
     }
     
     
@@ -25,10 +26,80 @@ class AuthenticationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.touchIDButton.isHidden = true
-        self.loginButton.isHidden = true
         
         self.loginView.layer.cornerRadius = 15
+        
+        self.authenticateUser()
     }
-
+    
+    
+    
+    
+    // MARK: - Authenticate
+    
+    func authenticateUser() {
+        let myContext = LAContext()
+        let myLocalizedReasonString = "This application is confidential. You need to be authenticate to use it !"
+        var authError: NSError?
+        
+        if myContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &authError) {
+            self.loginButton.isHidden = true
+            myContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: myLocalizedReasonString) { success, evaluateError in
+                if success {
+                    self.prepareForLigandTableView()
+                } else {
+                    if let error = evaluateError as? LAError {
+                        DispatchQueue.main.async {
+                            self.displayErrorMessage(error: error)
+                        }
+                    }
+                }
+            }
+        } else {
+            self.touchIDButton.isHidden = true
+        }
+    }
+    
+    func displayErrorMessage(error: LAError) {
+        var message = ""
+        
+        switch error.code {
+        case LAError.authenticationFailed:
+            message = "Authentication was not successful because the user failed to provide valid credentials."
+        case LAError.userCancel:
+            message = "Authentication was canceled by the user"
+        case LAError.userFallback:
+            message = "Authentication was canceled because the user tapped the fallback button"
+        case LAError.touchIDNotEnrolled:
+            message = "Authentication could not start because Touch ID has no enrolled fingers."
+        case LAError.passcodeNotSet:
+            message = "Passcode is not set on the device."
+        case LAError.systemCancel:
+            message = "Authentication was canceled by system"
+        default:
+            message = error.localizedDescription
+        }
+        
+        self.showAlertWith(title: "Authentication Failed", message: message)
+    }
+    
+    func showAlertWith(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let actionButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alertController.addAction(actionButton)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    // MARK: - Navigation
+    
+    func prepareForLigandTableView() {
+        let destinationVC = storyboard?.instantiateViewController(withIdentifier: "Ligand Table View") as! LigandTableViewController
+        
+        DispatchQueue.main.async { self.navigationController?.pushViewController(destinationVC, animated: true) }
+    }
+    
 }
